70c86abe10c068a80075a2252e76fdc2465481cd
lgoldstein <lgoldstein@groupon.com>
2015-08-15 14:36:06 -0700

    round4b

--- life.js
+++ life.js
@@ -1,13 +1,11 @@
 var grid = [];
 
 var gridIinit = function (grid_size){
-  grid_squares = []
   for (i in grid_size[0]) {
     for (j in grid_size[1]) {
-      grid_squares.push('X')//[i] = [i, j]
+      grid.push([i, j])
     }
   }
-  return grid_squares.push(grid);
 }
 
-console.log(gridIinit([1, 1]))
+gridIinit()
